dcc0596aea1dba806c943be7a3caa3342094cd60
Phoebe Manning <phoebe.manning8@gmail.com>
2017-09-20 22:57:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,4 @@
 function takeANumber(katzDeliLine, newCustomer){
   katzDeliLine.push(newCustomer);
   return(`Welcome, ${newCustomer}. You are number ${katzDeliLine.length} in line.`)
-    // for(let i=0; i<katzDeliLine.length; i++){
-    //   if(katzDeliLine[i] === newCustomer){
-    //
-    //   }
-    // }
 }
