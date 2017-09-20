a4d265e00690817801945d6737e889f6ccb13950
Phoebe Manning <phoebe.manning8@gmail.com>
2017-09-20 22:56:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 //takeANumber(katzDeliLine, "Ada"); //"Welcome, Ada. You are number 1 in line."
 function takeANumber(katzDeliLine, newCustomer){
-  
-  for(let i=0; i<katzDeliLine.length; i++){
-    if(katzDeliLine[i] === newCustomer){
-      
-    }
-  }
+  katzDeliLine.push(newCustomer);
+  return(`Welcome, ${newCustomer}. You are number ${katzDeliLine.length} in line.`)
+    // for(let i=0; i<katzDeliLine.length; i++){
+    //   if(katzDeliLine[i] === newCustomer){
+    // 
+    //   }
+    // }
 }
