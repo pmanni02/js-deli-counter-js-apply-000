18ee81eda7b9f1cedb2f65cfe8b7713daecbca86
Phoebe Manning <phoebe.manning8@gmail.com>
2017-09-20 22:58:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function takeANumber(katzDeliLine, newCustomer){
   katzDeliLine.push(newCustomer);
   return(`Welcome, ${newCustomer}. You are number ${katzDeliLine.length} in line.`)
 }
+
+//nowServing(katzDeliLine); // "Currently serving Ada."
+function nowServing(){
+
+}
